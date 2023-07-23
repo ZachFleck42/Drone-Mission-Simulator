0e7239450cd2c8a4d7de0acb802d564b1d52df0d
ZachFleck42 <zachfleck42@Gmail.com>
2023-07-23 19:58:56 -0400

    Initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+	importOrder: [
+		'^@core/(.*)$',
+		'^@server/(.*)$',
+		'^@ui/(.*)$',
+		'^\\$lib/components/(.*)$',
+		'^\\$lib/(.*)$',
+		'^[./]',
+	],
+	arrowParens: 'always',
+	bracketSameLine: true,
+	bracketSpacing: true,
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+	printWidth: 80,
+	proseWrap: 'always',
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'all',
+	useTabs: true,
+};
