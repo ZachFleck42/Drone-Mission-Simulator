420308703ba5819f43ce2851f6096461c95737c4
ZachFleck42 <zachfleck42@Gmail.com>
2023-07-24 11:46:50 -0400

    Adds tick simulation and moving target to simulator

--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -6,14 +6,32 @@ struct Tile {
     is_unsafe: bool,
 }
 
+struct Target {
+    x: usize,
+    y: usize,
+}
+
+#[derive(Clone, Copy)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
 fn generate_grid() -> Vec<Vec<Tile>> {
-    let mut grid = Vec::with_capacity(256);
+    let mut grid = Vec::with_capacity(16);
+
     let mut rng = rand::thread_rng();
 
     for i in 0..16 {
         let mut row = Vec::with_capacity(16);
         for j in 0..16 {
-            let tile_type = if rng.gen::<f64>() < 0.1 { true } else { false };
+            let tile_type = if rng.gen_range(0..10) < 1 {
+                true
+            } else {
+                false
+            };
 
             let tile = Tile {
                 x: i,
@@ -28,13 +46,61 @@ fn generate_grid() -> Vec<Vec<Tile>> {
     grid
 }
 
+fn move_target(target: &mut Target) {
+    let mut rng = rand::thread_rng();
+    if rng.gen_range(0..10) < 3 {
+        let mut valid_directions = Vec::new();
+
+        if target.x > 0 {
+            valid_directions.push(Direction::Up);
+        }
+        if target.x < 15 {
+            valid_directions.push(Direction::Down);
+        }
+        if target.y > 0 {
+            valid_directions.push(Direction::Left);
+        }
+        if target.y < 15 {
+            valid_directions.push(Direction::Right);
+        }
+
+        if !valid_directions.is_empty() {
+            let direction = valid_directions[rng.gen_range(0..valid_directions.len())];
+
+            match direction {
+                Direction::Up => target.x -= 1,
+                Direction::Down => target.x += 1,
+                Direction::Left => target.y -= 1,
+                Direction::Right => target.y += 1,
+            }
+        }
+    }
+}
+
 fn main() {
     let grid = generate_grid();
+    let mut target = Target {
+        x: rand::thread_rng().gen_range(0..16),
+        y: rand::thread_rng().gen_range(0..16),
+    };
 
-    for i in 0..16 {
-        for j in 0..16 {
-            let tile = &grid[i][j];
-            print!("({},{}) - {:?}  ", tile.x, tile.y, tile.is_unsafe);
+    // Simulate target movement for 10 ticks
+    for tick in 1..=10 {
+        move_target(&mut target);
+        println!("Tick {}: Target is at ({}, {})", tick, target.x, target.y);
+        for i in 0..16 {
+            for j in 0..16 {
+                let tile = &grid[i][j];
+                let symbol = if i == target.x && j == target.y {
+                    'T'
+                } else if tile.is_unsafe {
+                    'X'
+                } else {
+                    'O'
+                };
+                print!("{} ", symbol);
+            }
+            println!();
         }
         println!();
     }
