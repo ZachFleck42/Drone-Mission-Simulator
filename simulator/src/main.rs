bc5722a3ba54d8295fa1b78efaac34e21f7ab26b
ZachFleck42 <zachfleck42@Gmail.com>
2023-07-24 20:50:46 -0400

    Refactors terrain and target into Environment

--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -64,18 +64,98 @@ impl Terrain {
     }
 }
 
+impl Environment {
+    fn new(terrain: Terrain) -> Self {
+        let mut rng = rand::thread_rng();
+        let mut environment = Environment {
+            terrain,
+            target: Target {
+                x: 0,
+                y: 0,
+                name: None,
+            },
+        };
+
+        environment.target.x = rng.gen_range(0..environment.terrain.size);
+        environment.target.y = rng.gen_range(0..environment.terrain.size);
+
+        environment
+    }
+
+    fn move_target(&mut self) {
+        let mut rng = rand::thread_rng();
+        if rng.gen_range(0..10) < 5 {
+            let mut valid_directions = Vec::new();
+
+            if self.target.x > 0 {
+                valid_directions.push(Direction::Up);
+            }
+            if self.target.x < self.terrain.size - 1 {
+                valid_directions.push(Direction::Down);
+            }
+            if self.target.y > 0 {
+                valid_directions.push(Direction::Left);
+            }
+            if self.target.y < self.terrain.size - 1 {
+                valid_directions.push(Direction::Right);
+            }
+
+            if !valid_directions.is_empty() {
+                let direction = valid_directions[rng.gen_range(0..valid_directions.len())];
+
+                match direction {
+                    Direction::Up => self.target.x -= 1,
+                    Direction::Down => self.target.x += 1,
+                    Direction::Left => self.target.y -= 1,
+                    Direction::Right => self.target.y += 1,
+                }
+            }
+        }
+    }
+
+    fn set_target_name(&mut self, name: String) {
+        self.target.name = Some(name);
+    }
+
+    fn print(&self) {
+        for i in 0..self.terrain.size {
+            for j in 0..self.terrain.size {
+                let tile = &self.terrain.tiles[i][j];
+
+                let symbol = if tile.x == self.target.x && tile.y == self.target.y {
+                    'T'
+                } else if tile.hostile {
+                    'X'
+                } else {
+                    'O'
+                };
+                print!("{} ", symbol);
+            }
+            println!();
+        }
+    }
+}
+
 fn main() {
     let mut terrain = Terrain::new(8);
-    terrain.print();
+    let mut environment = Environment::new(terrain);
+
+    // Print initial environment
+    println!(
+        "Initial: Target is at ({}, {})",
+        environment.target.x, environment.target.y
+    );
+    environment.print();
+    println!();
 
     // Simulate target movement for 10 ticks
-    // for tick in 1..=10 {
-    //     terrain.move_target();
-    //     println!(
-    //         "Tick {}: Target is at ({}, {})",
-    //         tick, terrain.target.x, terrain.target.y
-    //     );
-    //     terrain.print();
-    //     println!();
-    // }
+    for tick in 1..=10 {
+        environment.move_target();
+        println!(
+            "Tick {}: Target is at ({}, {})",
+            tick, environment.target.x, environment.target.y
+        );
+        environment.print();
+        println!();
+    }
 }
