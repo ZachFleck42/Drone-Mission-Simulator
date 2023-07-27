6bf8944f42fc92867f5617f25efdfecd53616dc5
ZachFleck42 <zachfleck42@Gmail.com>
2023-07-27 10:35:00 -0400

    Refactors move_target() method to use get_surrounding_tiles(), removes DIRECTIONS enum

--- simulator/src/env.rs
+++ simulator/src/env.rs
@@ -1,5 +1,5 @@
+use crate::utils::get_surrounding_tiles;
 use crate::utils::random_choice;
-use crate::utils::DIRECTIONS;
 use rand::Rng;
 
 pub struct Tile {
@@ -97,22 +97,9 @@ impl Environment {
 
     pub fn move_target(&mut self) {
         if rand::thread_rng().gen_range(0..100) < self.target.move_rate {
-            let mut valid_moves = Vec::new();
-            let max_bound = self.terrain.size as i32;
-
-            for (dx, dy) in DIRECTIONS {
-                let new_x = self.target.x as i32 + dx;
-                let new_y = self.target.y as i32 + dy;
-
-                if new_x >= 0 && new_x < max_bound && new_y >= 0 && new_y < max_bound {
-                    valid_moves.push((dx, dy));
-                }
-            }
-
-            if !valid_moves.is_empty() {
-                let (dx, dy) = random_choice(&valid_moves);
-                self.target.x = (self.target.x as i32 + dx) as usize;
-                self.target.y = (self.target.y as i32 + dy) as usize;
+            let moves = get_surrounding_tiles(self.terrain.size, 1, self.target.x, self.target.y);
+            if !moves.is_empty() {
+                (self.target.x, self.target.y) = random_choice(&moves);
             }
         }
     }
--- simulator/src/utils.rs
+++ simulator/src/utils.rs
@@ -1,16 +1,5 @@
 use rand::Rng;
 
-pub static DIRECTIONS: [(i32, i32); 8] = [
-    (-1, -1),
-    (-1, 0),
-    (-1, 1),
-    (0, -1),
-    (0, 1),
-    (1, -1),
-    (1, 0),
-    (1, 1),
-];
-
 /// Returns a copy of a random element in the provided data structure.
 /// Structure must implement the 'Clone' trait.
 pub fn random_choice<T: Clone>(data: &[T]) -> T {
