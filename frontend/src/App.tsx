dcfba792a2a0908887891d33efe407f775a9422c
ZachFleck42 <zachfleck42@Gmail.com>
2023-07-30 12:46:31 -0400

    Makes test page look a little better

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,43 @@
+import React, { useState } from 'react';
 import GetButton from './lib/components/GetButton';
 import PostButton from './lib/components/PostButton';
+import './App.css';
 
 function App() {
-	const postData = {
-		test1: 'Hello',
-		test2: 'world!',
+	const [inputData, setInputData] = useState({
+		input1: '',
+		input2: '',
+	});
+
+	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = event.target;
+		setInputData((prevData) => ({
+			...prevData,
+			[name]: value,
+		}));
 	};
 
 	return (
 		<div className="App">
 			<p>Hello world!</p>
+			<div className="input-container">
+				<input
+					type="text"
+					name="test1"
+					value={inputData.input1}
+					onChange={handleInputChange}
+					placeholder="Enter input 1"
+				/>
+				<input
+					type="text"
+					name="test2"
+					value={inputData.input2}
+					onChange={handleInputChange}
+					placeholder="Enter input 2"
+				/>
+			</div>
 			<GetButton url="http://127.0.0.1:8080/" />
-			<PostButton url="http://127.0.0.1:8080/echo" postData={postData} />
+			<PostButton url="http://127.0.0.1:8080/echo" postData={inputData} />
 		</div>
 	);
 }
