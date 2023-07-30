0a5c7fe6ebfa7179abe7d68ffe0852b912a18d3d
ZachFleck42 <zachfleck42@Gmail.com>
2023-07-30 13:33:01 -0400

    Adds fields for all current simulation parameters

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,12 @@ import './App.css';
 
 function App() {
 	const [inputData, setInputData] = useState({
-		input1: '',
-		input2: '',
+		env_terrain_grid_size: '',
+		env_terrain_hostile_rate: '',
+		env_target_move_rate: '',
+		drone_move_range: '',
+		drone_vis_range: '',
+		sim_max_ticks: '',
 	});
 
 	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,18 +26,46 @@ function App() {
 			<p>Hello world!</p>
 			<div className="input-container">
 				<input
-					type="text"
-					name="input1"
-					value={inputData.input1}
+					type="number"
+					name="env_terrain_grid_size"
+					value={inputData.env_terrain_grid_size}
 					onChange={handleInputChange}
-					placeholder="Enter input 1"
+					placeholder="Terrain grid size"
 				/>
 				<input
-					type="text"
-					name="input2"
-					value={inputData.input2}
+					type="number"
+					name="env_terrain_hostile_rate"
+					value={inputData.env_terrain_hostile_rate}
 					onChange={handleInputChange}
-					placeholder="Enter input 2"
+					placeholder="Terrain hostile rate"
+				/>
+				<input
+					type="number"
+					name="env_target_move_rate"
+					value={inputData.env_target_move_rate}
+					onChange={handleInputChange}
+					placeholder="Target move rate"
+				/>
+				<input
+					type="number"
+					name="drone_move_range"
+					value={inputData.drone_move_range}
+					onChange={handleInputChange}
+					placeholder="Drone move range"
+				/>
+				<input
+					type="number"
+					name="drone_vis_range"
+					value={inputData.drone_vis_range}
+					onChange={handleInputChange}
+					placeholder="Drone visibility range"
+				/>
+				<input
+					type="number"
+					name="sim_max_ticks"
+					value={inputData.sim_max_ticks}
+					onChange={handleInputChange}
+					placeholder="Simulation ticks"
 				/>
 			</div>
 			<GetButton url="http://127.0.0.1:8080/" />
