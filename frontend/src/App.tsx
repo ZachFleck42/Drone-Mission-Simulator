ac74b4308c7953dc8d8a3ef117b92259cf0c6cc6
ZachFleck42 <zachfleck42@Gmail.com>
2023-07-30 15:09:03 -0400

    Input fields are smarter and display default values

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,18 +5,18 @@ import './App.css';
 
 function App() {
 	const [inputData, setInputData] = useState({
-		env_terrain_grid_size: '',
-		env_terrain_hostile_rate: '',
-		env_target_move_rate: '',
-		drone_move_range: '',
-		drone_vis_range: '',
-		sim_max_ticks: '',
+		env_terrain_grid_size: 0,
+		env_terrain_hostile_rate: 0,
+		env_target_move_rate: 0,
+		drone_move_range: 0,
+		drone_vis_range: 0,
+		sim_max_ticks: 0,
 	});
 
 	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		const { name, value } = event.target;
 
-		const numValue = parseInt(value, 10);
+		const numValue = value === '' ? 0 : parseInt(value, 10);
 
 		if (!isNaN(numValue) && numValue >= 0) {
 			setInputData((prevData) => ({
@@ -33,44 +33,60 @@ function App() {
 				<input
 					type="number"
 					name="env_terrain_grid_size"
-					value={inputData.env_terrain_grid_size}
+					value={
+						inputData.env_terrain_grid_size === 0
+							? ''
+							: inputData.env_terrain_grid_size
+					}
 					onChange={handleInputChange}
-					placeholder="Terrain grid size"
+					placeholder="Terrain grid size (default 16)"
 				/>
 				<input
 					type="number"
 					name="env_terrain_hostile_rate"
-					value={inputData.env_terrain_hostile_rate}
+					value={
+						inputData.env_terrain_hostile_rate === 0
+							? ''
+							: inputData.env_terrain_hostile_rate
+					}
 					onChange={handleInputChange}
-					placeholder="Terrain hostile rate"
+					placeholder="Terrain hostile rate (default 10)"
 				/>
 				<input
 					type="number"
 					name="env_target_move_rate"
-					value={inputData.env_target_move_rate}
+					value={
+						inputData.env_target_move_rate === 0
+							? ''
+							: inputData.env_target_move_rate
+					}
 					onChange={handleInputChange}
-					placeholder="Target move rate"
+					placeholder="Target move rate (default 10)"
 				/>
 				<input
 					type="number"
 					name="drone_move_range"
-					value={inputData.drone_move_range}
+					value={
+						inputData.drone_move_range === 0 ? '' : inputData.drone_move_range
+					}
 					onChange={handleInputChange}
-					placeholder="Drone move range"
+					placeholder="Drone move range (default 1)"
 				/>
 				<input
 					type="number"
 					name="drone_vis_range"
-					value={inputData.drone_vis_range}
+					value={
+						inputData.drone_vis_range === 0 ? '' : inputData.drone_vis_range
+					}
 					onChange={handleInputChange}
-					placeholder="Drone visibility range"
+					placeholder="Drone visibility range (default 2)"
 				/>
 				<input
 					type="number"
 					name="sim_max_ticks"
-					value={inputData.sim_max_ticks}
+					value={inputData.sim_max_ticks === 0 ? '' : inputData.sim_max_ticks}
 					onChange={handleInputChange}
-					placeholder="Simulation ticks"
+					placeholder="Simulation ticks (default 100)"
 				/>
 			</div>
 			<GetButton url="http://127.0.0.1:8080/hello" />
