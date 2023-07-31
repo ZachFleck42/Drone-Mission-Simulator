0f2257b50a1c4a802a52a7587dbf3888d9d766e0
ZachFleck42 <zachfleck42@Gmail.com>
2023-07-31 12:50:18 -0400

    Factors out WebGL components into new file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,11 @@
 import './App.css';
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import { Simulation } from './types/Simulation';
 import RunSimulation from './components/RunSimulation';
 import GridDisplay from './components/GridDisplay';
-import { createRoot } from 'react-dom/client';
-import { Canvas, useFrame, ThreeElements } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import * as THREE from 'three';
+import ZCanvas from './components/TestCanvas';
 
 const API = 'http://127.0.0.1:8080/sim';
-
-function Box(props: ThreeElements['mesh']) {
-	const meshRef = useRef<THREE.Mesh>(null!);
-	const [hovered, setHover] = useState(false);
-	const [active, setActive] = useState(false);
-	useFrame((state, delta) => (meshRef.current.rotation.x += delta));
-	return (
-		<mesh
-			{...props}
-			ref={meshRef}
-			scale={active ? 1.5 : 1}
-			onClick={(event) => setActive(!active)}
-			onPointerOver={(event) => setHover(true)}
-			onPointerOut={(event) => setHover(false)}>
-			<boxGeometry args={[1, 1, 1]} />
-			<meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-		</mesh>
-	);
-}
 
 function App() {
 	const [apiData, setApiData] = useState<Simulation>([]);
@@ -56,14 +34,7 @@ function App() {
 				<div style={{ marginTop: '100px' }}>Waiting for simulation data...</div>
 			)}
 			<div>
-				<Canvas>
-					<OrbitControls />
-					<ambientLight />
-					<axesHelper />
-					<pointLight position={[10, 10, 10]} />
-					<Box position={[-1.2, 0, 0]} />
-					<Box position={[1.2, 0, 0]} />
-				</Canvas>
+				<ZCanvas />
 			</div>
 		</div>
 	);
--- frontend/src/components/TestCanvas.tsx
+++ frontend/src/components/TestCanvas.tsx
@@ -1,56 +1,36 @@
-import { useRef, useEffect } from 'react';
 import * as THREE from 'three';
+import { useState, useRef } from 'react';
+import { Canvas, useFrame, ThreeElements } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
-function ThreeCanvas() {
-	const canvasRef = useRef<HTMLDivElement>(null);
-
-	useEffect(() => {
-		if (!canvasRef.current) return;
-
-		// Renderer
-		const renderer = new THREE.WebGLRenderer({ antialias: true });
-		renderer.setSize(window.innerWidth, window.innerHeight);
-		canvasRef.current.appendChild(renderer.domElement);
-
-		// Scene
-		const scene = new THREE.Scene();
-
-		// Camera
-		const camera = new THREE.PerspectiveCamera(
-			75,
-			window.innerWidth / window.innerHeight,
-			0.1,
-			1000,
-		);
-		camera.position.z = 5;
-
-		// Cube
-		const geometry = new THREE.BoxGeometry();
-		const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-		const cube = new THREE.Mesh(geometry, material);
-		scene.add(cube);
-
-		// Animation
-		const animate = () => {
-			requestAnimationFrame(animate);
-
-			// Rotate the cube
-			cube.rotation.x += 0.01;
-			cube.rotation.y += 0.01;
-
-			renderer.render(scene, camera);
-		};
-
-		animate();
-
-		return () => {
-			// Clean up resources when the component is unmounted
-			renderer.dispose();
-			canvasRef.current!.removeChild(renderer.domElement);
-		};
-	}, []);
-
-	return <div ref={canvasRef} />;
+function Box(props: ThreeElements['mesh']) {
+	const meshRef = useRef<THREE.Mesh>(null!);
+	const [hovered, setHover] = useState(false);
+	const [active, setActive] = useState(false);
+	useFrame((state, delta) => (meshRef.current.rotation.x += delta));
+	return (
+		<mesh
+			{...props}
+			ref={meshRef}
+			scale={active ? 1.5 : 1}
+			onClick={(event) => setActive(!active)}
+			onPointerOver={(event) => setHover(true)}
+			onPointerOut={(event) => setHover(false)}>
+			<boxGeometry args={[1, 1, 1]} />
+			<meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+		</mesh>
+	);
 }
 
-export default ThreeCanvas;
+export default function ZCanvas() {
+	return (
+		<Canvas>
+			<OrbitControls />
+			<ambientLight />
+			<axesHelper />
+			<pointLight position={[10, 10, 10]} />
+			<Box position={[-1.2, 0, 0]} />
+			<Box position={[1.2, 0, 0]} />
+		</Canvas>
+	);
+}
