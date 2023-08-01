4dc1d11fdb9167bdb1b0fffe4a0e09864cb14378
ZachFleck42 <zachfleck42@Gmail.com>
2023-07-31 21:54:32 -0400

    Begins to implement Grid display

--- frontend/src/components/CanvasDisplay.tsx
+++ frontend/src/components/CanvasDisplay.tsx
@@ -1,8 +1,12 @@
 import React, { useRef, useState } from 'react';
-import { Canvas, useFrame, ThreeElements } from '@react-three/fiber';
+import { Canvas, useFrame, ThreeElements, MeshProps } from '@react-three/fiber';
 import * as THREE from 'three';
 
-function Tile(props: ThreeElements['mesh'], size: number, color: string) {
+interface GridTileProps extends MeshProps {
+	color: string;
+}
+
+function GridTile({ color, ...props }: GridTileProps) {
 	const meshRef = useRef<THREE.Mesh>(null!);
 	const [hovered, setHover] = useState(false);
 	const [active, setActive] = useState(false);
@@ -13,7 +17,7 @@ function Tile(props: ThreeElements['mesh'], size: number, color: string) {
 			onClick={(event) => setActive(!active)}
 			onPointerOver={(event) => setHover(true)}
 			onPointerOut={(event) => setHover(false)}>
-			<planeGeometry args={[size, size]} />
+			<planeGeometry />
 			<meshStandardMaterial color={color} side={THREE.DoubleSide} />
 		</mesh>
 	);
