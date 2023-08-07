9a125731e10d26ea409a591eed0628c0d35856b8
ZachFleck42 <zachfleck42@Gmail.com>
2023-08-06 23:19:45 -0400

    Adds a custom model for the target object

--- frontend/src/components/WebGL/Target.tsx
+++ frontend/src/components/WebGL/Target.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { MeshProps, useFrame } from '@react-three/fiber';
+import { useGLTF } from '@react-three/drei';
 import { Environment } from '../../types/Simulation';
+import ToySoldierModel from '../../assets/models/toy_soldier.glb';
 
 interface TargetProps extends MeshProps {
 	environment: Environment;
@@ -13,6 +15,7 @@ export default function TargetMesh({
 	targetRef,
 	showAnimation,
 }: TargetProps) {
+	const model: any = useGLTF(ToySoldierModel);
 	const [position, setPosition] = useState({ x: 0, y: 0 });
 
 	useFrame((_, delta) => {
@@ -22,16 +25,17 @@ export default function TargetMesh({
 			const newX = position.x + (x - position.x) * speed * delta;
 			const newY = position.y + (-y - position.y) * speed * delta;
 			setPosition({ x: newX, y: newY });
-			targetRef.current.position.set(newX, 0.5, newY);
+			targetRef.current.position.set(newX, 0.79, newY);
 		} else {
-			targetRef.current.position.set(x, 0.5, -y);
+			targetRef.current.position.set(x, 0.79, -y);
 		}
 	});
 
 	return (
-		<mesh castShadow ref={targetRef}>
-			<boxGeometry args={[0.5, 1, 0.5]} />
-			<meshStandardMaterial color={'orange'} />
-		</mesh>
+		// <mesh castShadow ref={targetRef}>
+		// 	<boxGeometry args={[0.5, 1, 0.5]} />
+		// 	<meshStandardMaterial color={'orange'} />
+		// </mesh>
+		<primitive object={model.scene} ref={targetRef} scale={0.01} />
 	);
 }
