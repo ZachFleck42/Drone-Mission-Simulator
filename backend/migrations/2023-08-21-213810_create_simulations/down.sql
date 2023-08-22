d323a9ea9fd997d511c062638f9b89e247ce6286
ZachFleck42 <zachfleck42@Gmail.com>
2023-08-21 22:33:29 -0400

    Adds table creation to Diesel migrations

--- backend/migrations/2023-08-21-213810_create_simulations/down.sql
+++ backend/migrations/2023-08-21-213810_create_simulations/down.sql
@@ -1,1 +1,3 @@
--- This file should undo anything in `up.sql`
+DROP TABLE simulations;
+
+DROP TABLE simulation_frames;
--- backend/migrations/2023-08-21-213810_create_simulations/up.sql
+++ backend/migrations/2023-08-21-213810_create_simulations/up.sql
@@ -1,1 +1,19 @@
--- Your SQL goes here
+CREATE TABLE simulations (
+  id SERIAL PRIMARY KEY,
+  generated_id VARCHAR NOT NULL,
+  generated_time BIGINT NOT NULL,
+  no_frames INT NOT NULL,
+  grid_size INT NOT NULL,
+  hostile_rate SMALLINT NOT NULL,
+  move_rate SMALLINT NOT NULL,
+  drone_move_range SMALLINT NOT NULL,
+  drone_vis_range SMALLINT NOT NULL
+);
+
+CREATE TABLE simulation_frames (
+    id SERIAL PRIMARY KEY,
+    sim_id INT REFERENCES simulations(id),
+    frame_no INT NOT NULL,
+    environment JSONB NOT NULL,
+    drone JSONB NOT NULL
+)
